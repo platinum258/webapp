5409a09a92651319da408b6932435419c039e761
platinum258 <szq1786340401@icloud.com>
2024-01-22 21:49:33 +0900

    add records and foods table

---
+++ database/migrations/2024_01_22_124322_create_foods_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('foods', function (Blueprint $table) {
+            $table->id();
+            $table->string('food_name_various',50);
+            $table->string('material_name',50);
+            $table->string('material_id',50);
+            $table->integer('material_weight');
+            $table->integer('default_material_weight');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('foods');
+    }
+};
